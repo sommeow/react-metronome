7e00317b7b202882b83e2bec40317508f6aaaa27
sommeow <s.pimson@gmail.com>
2019-07-08 15:37:56 -0500

    addition of sound functionality

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import click1 from './click1.wav'
+import click2 from './click2.wav'
 import './Metronome.css'
 
 class Metronome extends Component {
     constructor(props) {
         super(props)
+        this.click1 = new Audio(click1)
+        this.click2 = new Audio(click2)
 
         this.state = {
             playing: false,
@@ -11,13 +15,48 @@ class Metronome extends Component {
             bpm: 100,
             beatsPerMeasure: 4
         }
+        
     }
 
     handleBpmChange = event => {
         const bpm = event.target.value;
         this.setState({ bpm })
     }
-    
+
+    startStop = () => {
+        if (this.state.playing) {
+            clearInterval(this.timer)
+            this.setState({
+                playing: false
+            })
+        } else {
+            this.timer = setInterval(
+                this.playClick,
+                (60 / this.state.bpm) * 1000
+            )
+            this.setState(
+                {
+                    count: 0,
+                    playing: true
+                },
+                this.playClick
+            )
+        }
+    }
+
+    playClick = () => {
+        const { count, beatsPerMeasure } = this.state
+
+        if (count % beatsPerMeasure === 0 ) {
+            this.click2.play()
+        } else {
+            this.click1.play()
+        }
+
+        this.setState(state => ({
+            count: (state.count +1) % state.beatsPerMeasure
+        }))
+    }
     
     render() {
         const { playing, bpm } = this.state
@@ -28,7 +67,7 @@ class Metronome extends Component {
             <div>{bpm} BPM</div>
             <input type="range" min="60" max="240" value={bpm} onChange={this.handleBpmChange} />
             </div>
-            <button>{playing ? 'Stop' : 'Start'} </button>
+            <button onClick={this.startStop}>{playing ? 'Stop' : 'Start'} </button>
         </div>
         )
     }
